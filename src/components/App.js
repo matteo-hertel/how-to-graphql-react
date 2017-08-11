51b1829809c5f4f7e62b4bf93b158dcbf43ea3b3
Matteo Hertel <info@matteohertel.com>
2017-08-11 17:35:35 +0200

    add login component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Login from './Login'
 import './App.css';
 
 class App extends Component {
@@ -12,8 +13,9 @@ class App extends Component {
         <Header />
         <div className='ph3 pv1 background-gray'>
           <Switch>
-            <Route exact path='/' component={LinkList} />
+            <Route exact path='/login' component={Login} />
             <Route exact path='/create' component={CreateLink} />
+            <Route exact path='/' component={LinkList} />
           </Switch>
         </div>
       </div>
